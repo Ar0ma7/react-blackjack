2a03b5d9bd3efb1d903f9f170370fcb67a198992
Aroma <aroma.xxx.777@gmail.com>
2024-06-06 05:46:24 +0900

    fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,22 +1,27 @@
-import { memo, useEffect } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import { styles } from './Card.css';
 import { Card as CardType } from '@/types/index';
 
 type Props = CardType;
 
-const preloadImage = (src: string) => {
-  const img = new Image();
-  img.src = src;
-};
-
 export const Card = memo(({ suite, number, isFront }: Props) => {
-  const imgSrc = isFront
-    ? `/images/card_${suite}_${`${number}`.padStart(2, '0')}.png`
-    : '/images/card_back.png';
+  const [imgSrc, setImgSrc] = useState('');
+
+  const loadImage = useCallback(async () => {
+    if (isFront) {
+      const image = await import(
+        `/images/card_${suite}_${`${number}`.padStart(2, '0')}.png`
+      );
+      setImgSrc(image.default);
+    } else {
+      const image = await import('/images/card_back.png');
+      setImgSrc(image.default);
+    }
+  }, [isFront, number, suite]);
 
   useEffect(() => {
-    preloadImage(imgSrc);
-  }, [imgSrc]);
+    loadImage();
+  }, [suite, number, isFront, loadImage]);
 
   return (
     <div css={styles.card}>
