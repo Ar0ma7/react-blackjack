fb65389e38c61db7e1850f08d5765140cdac242a
Aroma <aroma.xxx.777@gmail.com>
2024-06-06 06:47:45 +0900

    fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { styles } from './Card.css';
 import { image } from './image';
 import { Card as CardType } from '@/types/index';
@@ -6,13 +6,24 @@ import { Card as CardType } from '@/types/index';
 type Props = CardType;
 
 export const Card = memo(({ suite, number, isFront }: Props) => {
+  const [loaded, setLoaded] = useState(false);
   const imgSrc = isFront
     ? image[`card_${suite}_${`${number}`.padStart(2, '0')}`]
     : image.card_back;
 
+  useEffect(() => {
+    const img = new Image();
+    img.src = imgSrc;
+
+    img.onload = () => {
+      setLoaded(true);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div css={styles.card}>
-      <img src={imgSrc} alt="" css={styles.image} />
+      {loaded && <img src={imgSrc} alt="" css={styles.image} />}
     </div>
   );
 });
