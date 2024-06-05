ad5af061228640844bd7bdd4eaf12100bfe5a26c
Aroma <aroma.xxx.777@gmail.com>
2024-06-06 05:34:25 +0900

    fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,26 @@
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { styles } from './Card.css';
 import { Card as CardType } from '@/types/index';
 
 type Props = CardType;
+
+const preloadImage = (src: string) => {
+  const img = new Image();
+  img.src = src;
+};
 
 export const Card = memo(({ suite, number, isFront }: Props) => {
   const imgSrc = isFront
     ? `/images/card_${suite}_${`${number}`.padStart(2, '0')}.png`
     : '/images/card_back.png';
 
+  useEffect(() => {
+    preloadImage(imgSrc);
+  }, [imgSrc]);
+
   return (
     <div css={styles.card}>
-      <img src={imgSrc} alt="" css={styles.image} />
+      <img src={imgSrc} alt="" css={styles.image} loading="lazy" />
     </div>
   );
 });
